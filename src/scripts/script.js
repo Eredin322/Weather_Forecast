cabb0c8d71768dddee70fa10cd5cde9a173dfd6b
Eredin322 <bkmrfrbkmrf326915@mail.ru>
2022-11-05 12:29:39 +0600

    Add login modal with imput fields, submit button and show password button

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -267,3 +267,9 @@ function fillTheCard(index){
     }
 
 }
+
+const togglePassword = (button) => {
+    button.classList.toggle("showing");
+    const input = document.getElementById("password");
+    input.type = input.type === "password" ? "text" : "password";
+};
